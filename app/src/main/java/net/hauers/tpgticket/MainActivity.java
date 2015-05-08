01049cdf4978b3301338e4e56e5f0a4b93606b98
Tamás Hauer <Tamas.Hauer@mcmri.com>
2015-05-08 17:38:22 +0200

    SIM unlock now working

--- app/src/main/java/net/hauers/tpgticket/MainActivity.java
+++ app/src/main/java/net/hauers/tpgticket/MainActivity.java
@@ -1,6 +1,7 @@
 package net.hauers.tpgticket;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -8,6 +9,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class MainActivity extends Activity {
@@ -46,5 +53,39 @@ public class MainActivity extends Activity {
             smsManager.sendTextMessage("788", null, "tpg2", null, null);
         }
         checkBox.setChecked(false);
+    }
+
+    public void unlockWiFi(View view) {
+        Log.i("net.hauers.tpgticket", "In unlockWifi()");
+
+        new AsyncTask<Void,Void,Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                Log.i("net.hauers.tpgticket", "In doInBackground()");
+                try {
+                    String urlParameters = "Pin_Verify=6666&save=%20Apply";
+                    URL url = new URL("http://10.15.17.1/goform/Login_Verify_PIN");
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                    writer.write(urlParameters);
+                    writer.flush();
+
+                    String line;
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = reader.readLine()) != null) {
+                        Log.i("net.hauers.tpgticket", line);
+                    }
+
+                    writer.close();
+                    reader.close();
+
+                } catch (Exception e) {
+                    Log.e("net.hauers.tpgticket", e.toString());
+                }
+                return null;
+            }
+        }.execute();
     }
 }
